3309ad3ac6e5cb3f3f41237577fbfc49512327ba
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-04-24 11:50:52 -0500

    commit tree b27765fddd87954363ba9d646c6758e7177cfc88

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,19 @@
+class StudentsController < ApplicationController
+
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades 
+        # byebug
+        students = Student.all.order('grade DESC')
+        render json: students 
+    end
+
+    def highest_grade
+        student = Student.all.order('grade DESC').first
+        render json: student 
+    end
+
+end
